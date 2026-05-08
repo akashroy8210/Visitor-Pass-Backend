5deb1e557c7db2de4d2b6e108651138e864a7d61
akashroy8210 <akash.roy@iitg.ac.in>
2026-05-08 07:39:49 +0530

    Update AuthRoutes.js

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,8 +1,9 @@
 const express=require("express")
-const {signupUser,verifyUser,loginUser}=require("../controllers/AuthControllers")
+const {signupUser,verifyUser,loginUser, forgetPassword, findUser}=require("../controllers/AuthControllers")
 const router=express.Router()
 router.post('/signup',signupUser)
 router.post('/signup/verify-otp',verifyUser)
 router.post('/login',loginUser)
-
+router.post('/login/forgetPass',forgetPassword)
+router.post('/login/findUser',findUser)
 module.exports=router

7882487ad60dc827c48d6bcdc587f626ad4cc6f3
akashroy8210 <akashroy73826@gmail.com>
2026-05-21 12:23:58 +0530

    fix the mailing email

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -1,30 +1,34 @@
-const nodemailer=require("nodemailer")
-const otpGenerator=require("otp-generator")
+const nodemailer = require("nodemailer")
+const otpGenerator = require("otp-generator")
 
-exports.generateOTP=()=>{
-    return otpGenerator.generate(6,{
-        digits:true,
-        lowerCaseAlphabets:false,
-        upperCaseAlphabets:false,
-        specialChars:false
+exports.generateOTP = () => {
+    return otpGenerator.generate(6, {
+        digits: true,
+        lowerCaseAlphabets: false,
+        upperCaseAlphabets: false,
+        specialChars: false
     })
 }
-exports.sendEmail=async ({to,subject,html})=>{
-    try{
-        const transporter= nodemailer.createTransport({
-            service:"gmail",
-            auth:{
-                user:process.env.EMAIL,
-                pass:process.env.EMAIL_PASS
-            }
-        })
+
+const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
+    },
+})
+exports.sendEmail = async ({ to, subject, html }) => {
+    try {
         await transporter.sendMail({
-            from:process.env.EMAIL,
+            from: `My Office <${process.env.EMAIL}>`,
             to,
             subject,
             html
         })
-    }catch(error){
+    } catch (error) {
         console.log(error)
+        throw error
     }
 }
